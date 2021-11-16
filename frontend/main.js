6814ca37b27cfec06dbfa4dfa27e30ef439b289a
ViriatoNova <viriatosampaio@gmail.com>
2021-11-16 09:50:26 -0300

    #16 - Ajust front-end

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,3 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './modules/cardStyle';
-
-
-// function findEmail() {
-//     const btnEdit = document.querySelectorAll('.edit-contact');
-//     btnEdit.forEach(element => {
-//         element.addEventListener('click', () => {
-//             alert('okays');
-//         });
-//     }); 
-// }
-// findEmail();
-
-
-
